15aa8ce8b5201bbec41cd1b272a36cbaf43a36ca
Partha Sahoo <M1049050@mindtree.com>
2019-08-08 11:33:27 +0530

    PassengerData

---
+++ src/app/Entity/Passenger.ts
@@ -0,0 +1,13 @@
+export interface Passenger{
+    name:string,
+    email:string,
+    passport:string,
+    address:string,
+    dob:string,
+    disability:string,
+    food:boolean,
+    seatnumber:number,
+    ischeckedin:boolean,
+    infants:number,
+    ancilarservices:boolean,
+}
--- src/app/airline.service.ts
+++ src/app/airline.service.ts
@@ -1,7 +1,65 @@
 import { Subject } from 'rxjs';
+import { Passenger } from './Entity/Passenger';
 
 export class AirlineService{
     showLogoutEvent=new Subject<String>();
+
+    passengerData:Passenger[]=[
+        {
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },
+        {
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },{
+            name:'String', email:'String', passport:'String', address:'String', dob:'String', disability:'String', food:true, seatnumber:1, ischeckedin:true, infants:2, ancilarservices:true,
+        },
+]
 
     showLogout(email:String){
         this.showLogoutEvent.next(email);
